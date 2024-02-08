933208199d023c9020da07b7de33b319881f125d
Kristoffer Wallqvist <112932893+KristofferGW@users.noreply.github.com>
2024-02-08 23:36:27 +0100

    Introduces routes and privacy page (not working)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,18 @@
 import './App.css';
-import React, { useState } from 'react';
-import Header from './components/Header';
-import SwapForm from './components/SwapForm';
-import SwapReults from './components/SwapResults';
-import StyledContainer from './styling-components/StyledContainer';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from "./pages/Home";
+import Privacy from "./pages/Privacy";
 
 function App() {
-  const [selectedChain, setSelectedChain] = useState("");
-
   return (
-    <div>
-      <Header />
-      <StyledContainer>
-        <SwapForm setSelectedChain={setSelectedChain} />
-        {selectedChain && <SwapReults selectedChain={selectedChain} />}
-      </StyledContainer>
-    </div>
-  );
+    <Router>
+      <Routes>
+        <Route path='/' exact component={<Home />} />
+        <Route path='/privacy' component={<Privacy />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
